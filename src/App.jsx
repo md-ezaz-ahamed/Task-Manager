da3c41d79e824d4ba7db690f385a612bd5130997
Ezaz Ahamed <ezazahamed3@gmail.com>
2025-05-13 00:07:21 +0600

    [SETUP]: Project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './shared/NavBar';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/shared/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../assets/logo.png';
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+		<div className="container mx-auto flex items-center justify-between gap-x-6">
+			<a href="/">
+			
+				<img className="h-[60px]" src={Logo} alt="Logo" />
+			</a>
+			
+		</div>
+	</nav>
+        </div>
+    );
+};
+
+export default NavBar;
