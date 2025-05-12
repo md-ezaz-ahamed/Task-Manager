d7fadc41a89ef6777dfd26e30c19a34f367aaee0
Ezaz Ahamed <ezazahamed3@gmail.com>
2025-05-13 00:22:47 +0600

    [fea]: Hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import NavBar from './shared/NavBar';
+import Footer from './shared/Footer';
+import Hero from './Hero-Section';
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <div className="bg-[#191D26] flex flex-col items-center justify-center">
+      <Hero />
+      <Footer />
+      </div>
+      
+      
     </div>
   );
 };
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+            <div>
+			<p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+				Copyright ©2025 | All rights reserved
+			</p>
+		</div>
         </div>
     );
 };
